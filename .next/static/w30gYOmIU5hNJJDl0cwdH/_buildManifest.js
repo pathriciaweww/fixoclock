311caf8f925e1bc8cb65fbb0e36580963c5a2562
pathriciaweww <pathriciawe@gmail.com>
2023-02-26 23:32:50 +1300

    update

---
+++ .next/static/w30gYOmIU5hNJJDl0cwdH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-7a1378f6d8b96a7f.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
